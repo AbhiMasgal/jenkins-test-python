b097ea196c7d9ec1350b8f2489f77e42225c2a6d
AbhiMasgal <masgalsonu@gmail.com>
2025-11-03 10:25:09 -0800

    To check Jenkin error email notification

--- test_jenkins.py
+++ test_jenkins.py
@@ -1,1 +1,1 @@
-print("Hello, welcome to test Jenkins for DevOps and Github and Next to check the edit files!")
+print"Hello, welcome to test Jenkins for DevOps and Github and Next to check the edit files!")
